39b0551ecd84d3eeed517e2b902ec0a88f66690c
Andrew <akcrono@gmail.com>
2014-08-21 15:39:40 -0400

    added dublicate url checking and more descriptive error messages

--- server.rb
+++ server.rb
@@ -11,9 +11,17 @@ def get_articles
   articles
 end
 
+def already_submitted post_url
+  articles = []
+  CSV.foreach('articles.csv', headers: true, header_converters: :symbol, converters: :all) do |row|
+    return true if row[:url] == post_url
+  end
+  false
+end
+
 def post_is_valid?(post_title, post_url, post_description)
 
-  if post_title == nil
+  if post_title == ''
     return false
 
   elsif post_url !~ (/^(www)\.\w+\..{2,6}$/)
@@ -21,14 +29,35 @@ def post_is_valid?(post_title, post_url, post_description)
 
   elsif post_description == nil || post_description.length < 20
     return false
+  elsif already_submitted post_url
+    return false
   end
   true
+end
 
+def title_is_valid? post_title
+  if post_title == ''
+    return false
+  end
+  true
+end
+
+def url_is_valid? post_url
+  if post_url !~ (/^(www)\.\w+\..{2,6}$/)
+    return false
+  end
+  true
+end
+
+def description_is_valid? post_description
+  if post_description == nil || post_description.length < 20
+    return false
+  end
+  true
 end
 
 
 
-########ROUTES############
 get '/' do
   @articles = get_articles
   erb :index
@@ -45,10 +74,20 @@ post '/submit' do
     end
   redirect '/'
   else
+
     @error = 'Invalid input'
     @post_title = params[:post_title]
     @post_url = params[:post_url]
     @post_description = params[:post_description]
+    if !title_is_valid? @post_title
+      @error = 'Invalid title'
+    elsif !url_is_valid? @post_url
+      @error = 'Invalid url (FORMAT: www.google.com'
+    elsif !description_is_valid? @post_description
+      @error = 'Description must be at least 20 characters'
+    else
+      @error = 'This URL has already been posted'
+    end
     erb :submit
   end
 end
